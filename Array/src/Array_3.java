eb5e66acd65a268c9c36a092ec5b6199fef3f59d
Sandeep Prajapati <sandeepstructure@gmail.com>
2020-11-15 00:34:38 +0530

    loop in each concept.

---
+++ Array/src/Array_3.java
@@ -0,0 +1,16 @@
+
+public class Array_3 
+{
+	public static void main(String[] args)
+	{
+		//create an array
+		int[] age= {12,4,5,4,7,84,};
+		System.out.println("Using for-each Loop: ");
+		for(int a:age)
+		{
+			System.out.println(a);
+		}
+		
+	}
+
+}
