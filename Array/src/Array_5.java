6b41ae072a724386ae0643a79bf1b9bf7235ab84
Sandeep Prajapati <sandeepstructure@gmail.com>
2020-11-15 00:42:24 +0530

    first print the length and then initialize the array

---
+++ Array/src/Array_5.java
@@ -0,0 +1,19 @@
+
+public class Array_5
+{
+	public static void main(String[] args)
+	{
+		int arr[]= {4,7,2,21321,654,123321};
+		int length=arr.length;
+		System.out.println(length);
+		
+		int arr2[]=new int[10];
+		arr2[2]=4;
+		int length2=arr2.length;
+		System.out.println(length2);
+		//will return 10. all other digits will be 0 initialize 
+		//because staic memory initialization
+	}
+
+
+}
