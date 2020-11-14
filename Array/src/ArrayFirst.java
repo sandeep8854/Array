38bfef868bea9ddb025ef560dcd0f9503962b0f2
Sandeep Prajapati <sandeepstructure@gmail.com>
2020-11-15 00:27:52 +0530

    Just first array program to find all the information

---
+++ Array/src/ArrayFirst.java
@@ -0,0 +1,17 @@
+
+public class ArrayFirst 
+{
+public static void main(String[] args)
+{
+	//creata a array
+	int[] age= {12,4,5,2,5};
+	//access each array element
+	System.out.println("Access  Element of Array :");
+	System.out.println("First Element : " +age[0]);
+	System.out.println("Second Element " +age[1]);
+	System.out.println("Third Element " +age[2]);
+	System.out.println("Fourth element "+age[3]);
+	System.out.println("vth element " +age[4]);
+	
+     }
+}
