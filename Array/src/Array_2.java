c41a376d4ff3245352d3ccc8ab0d47ec90621b5a
Sandeep Prajapati <sandeepstructure@gmail.com>
2020-11-15 00:32:01 +0530

    Flow of loop in array.print the looping concept.

---
+++ Array/src/Array_2.java
@@ -0,0 +1,17 @@
+
+public class Array_2 
+{
+	public static void main(String[] args)
+	{
+		//create an array
+		int[] age= {12,4,5};
+		//loop through the array
+		//using for loop
+		System.out.println("Using For Loop:");
+		for(int i=0;i<age.length;i++)
+		{
+			System.out.println(age[i]);
+		}
+	}
+
+}
